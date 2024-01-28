98bc8dcd0b46393d5dba416543a84f3c9b1d623f
yshareef <yanal.shareef@foothillsolutions.com>
2024-01-29 00:44:02 +0200

    week1 files and lessons

---
+++ 2_DOCKER_SQL/nyc.py
@@ -0,0 +1,61 @@
+import os 
+import pandas as pd
+import pyarrow.parquet as pq
+from sqlalchemy import create_engine
+import numpy as np
+from time import time
+import argparse
+
+def main(args):
+    
+    user = args.user
+    password = args.password
+    host = args.host 
+    port =  args.port 
+    database_name = args.database_name
+    table_name = args.table_name
+    file_url = args.file_url
+    csv_name = "output.parqet"
+
+    # download the csv file 
+    os.system(f"wget {file_url} -O {csv_name}")
+
+    # Read Parquet file into Pandas DataFrame
+    trips = pq.read_table(args.fileurl)
+    data = trips.to_pandas()
+
+    # Create a PostgreSQL engine using the provided arguments
+    engine_url = f"postgresql://{args.user}:{args.password}@{args.host}:{args.port}/{args.databasename}"
+    engine = create_engine(engine_url)
+
+    # Split the DataFrame into 100 chunks
+    dfs = np.array_split(data, 100)
+
+    # Create an empty table in the database for the first chunk (replace if exists)
+    dfs[0].head(0).to_sql(name=args.tablename, con=engine, if_exists="replace")
+
+    # Insert each chunk into the database with append mode
+    for df in dfs:
+        t_start = time()
+        df.to_sql(name=args.tablename, con=engine, if_exists="append")
+        t_end = time()
+        print("Inserted another chunk..., took %.3f seconds" % (t_end - t_start))
+
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Ingest parquet file to postgres')
+
+    # Add the desired command line arguments
+    parser.add_argument('--user', type=str, help='Postgres user')
+    parser.add_argument('--password', type=str, help='Postgres password')
+    parser.add_argument('--host', type=str, help='Postgres host')
+    parser.add_argument('--port', type=int, help='Postgres port')
+    parser.add_argument('--databasename', type=str, help='Postgres database name')
+    parser.add_argument('--tablename', type=str, help='Postgres table name')
+    parser.add_argument('--fileurl', type=str, help='URL of the Parquet file')
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Call the main function with the parsed arguments
+    main(args)
---
+++ 2_homework/variables.tf
@@ -0,0 +1,30 @@
+variable "project" {
+  description = "Project "
+  default     = "de-zoomcamp-412612"
+}
+
+variable "region" {
+  description = "Region "
+  default     = "us-central1"
+}
+
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "de-zoomcamp-412612-terra-bucket"
+}
+
+variable "google_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
